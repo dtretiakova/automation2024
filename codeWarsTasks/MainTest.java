fdad1e49384bb206e3b31650484955679d3ad428
dtretiakova <146701860+dtretiakova@users.noreply.github.com>
2024-11-09 16:28:25 +0200

    Delete codeWarsTasks directory

--- codeWarsTasks/MainTest.java
+++
@@ -1,9 +0,0 @@
-package codeWarsTasks;
-
-public class MainTest {
-    public static void main(String[] args) {
-        Task3 task3 = new Task3();
-        System.out.println(task3.toCamelCase("the-stealth-warrior"));
-
-    }
-}
